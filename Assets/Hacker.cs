1917c7cd2e46c12d4406f9b458f9c4d193d20cce
Srinivas Annapantula <ssk5492@gmail.com>
2018-03-09 20:43:57 -0800

    Set Game States

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,27 +1,74 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Hacker : MonoBehaviour {
+
+    //Game State
+    int level;
+    enum Screen { MainMenu, Password, Win};
+    Screen currentScreen = Screen.MainMenu;
 
 	// Use this for initialization
 	void Start () {
         ShowMainMenu();
     }
 
-    void ShowMainMenu() {
+    void ShowMainMenu()
+    {
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
-        string greeting = "Hello Hacker!!";
-        Terminal.WriteLine(greeting);
         Terminal.WriteLine("What would you like to hack into?\n");
         Terminal.WriteLine("Press 1 for the local library");
         Terminal.WriteLine("Press 2 for the police station");
         Terminal.WriteLine("Press 3 for NASA \n");
         Terminal.WriteLine("Enter your selection: ");
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else if (input == "007")
+        {
+            Terminal.WriteLine("Please select a valid level Mr. Bond.");
+        }
+        else
+        {
+            Terminal.WriteLine("Please enter a valid input");
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("You have chosen level " + level);
+    }
 }
